9b12288c6715e88fcdc5ada7bd8717474b2b365e
Liliana Gallegos <lilianac.gallegos@colostate.edu>
2023-03-22 11:07:22 -0600

    additional package installation

---
+++ setup_for_allof.r
@@ -0,0 +1,11 @@
+#!/usr/local/bin/Rscript
+
+##################################################
+#### REGGAE - Regression Generator & Analyzer ####
+#### SETUP code                               ####
+#### AUTHOR: Liliana C. Gallegos              ####
+#### EMAIL: lilianac.gallegos@colostate.edu   ####
+##################################################
+
+
+install.packages("tidyselect", repos="https://cran.revolutionanalytics.com/")
